0f62f770b74f619ae80d8620f8ad0a6f4ef46004
Martin Beeby <martinbeeby@hotmail.com>
2017-08-17 00:19:58 +0100

    Hard Coding routes

--- ExtractTextFromPdf/index.js
+++ ExtractTextFromPdf/index.js
@@ -1,15 +1,16 @@
 module.exports = function (context, myBlob) {
     var fs = require('fs');
+    var workingDirectory = "D:\\home\\site\\wwwroot\\"
 
     context.log("Trigger from git \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
      const { spawn } = require('child_process');
     //const bat = exec('cmd.exe', ['D:\home\site\wwwroot\BlobTriggerJS1\pdftotext.exe -h']);
 
-    fs.writeFile("D:\\home\\site\\wwwroot\\sample.pdf", myBlob, function (err) {
+    fs.writeFile(workingDirectory + "sample.pdf", myBlob, function (err) {
         if (err) return context.log(err); context.done();
         context.log('File Written');
 
-    var cp = spawn(process.env.comspec, ['/c', 'pdftotext.exe -nodiag sample.pdf sample.txt']);
+    var cp = spawn(process.env.comspec, ['/c', `${workingDirectory}pdftotext.exe -nodiag ${workingDirectory}sample.pdf ${workingDirectory}sample.txt`]);
 
     cp.stdout.on("data", function(data) {
         var str = "Out: " + data.toString();
@@ -28,7 +29,7 @@ module.exports = function (context, myBlob) {
     cp.on('close', (code) => {
         context.log(`child process exited with code ${code}`);
         
-        var array = fs.readFileSync('sample.txt').toString().split("\n");
+        var array = fs.readFileSync(`${workingDirectory}sample.txt`).toString().split("\n");
         for(i in array) {
             context.log(array[i]);
         }
