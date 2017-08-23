6a825f322d8fccd9fea6705095a80e1adfc0f2c8
Anita Ramanan <anraman@microsoft.com>
2017-08-23 02:13:38 +0100

    Added keyword extraction for questions, answers and full document

--- python/extractKeywords.py
+++ python/extractKeywords.py
@@ -2,30 +2,53 @@ import urllib.request
 import spacy
 import re
 from collections import Counter
+import json
+
 nlp = spacy.load("en")
 
 def populateStopwords(stopwordURL):
-    stopwords = urllib.request.urlopen(stopwordURL).read().decode("utf8")
-    stopwords = stopwords.replace('\n', '').replace('"', '').split("\r")
-    return stopwords
+    stopwords = getTextFromURL(stopwordURL)
+    stopwords = stopwords.replace('"', '').replace('\n', '').split("\r")
 
-def extractKeywords(documentURL):
     for line in stopwords:
         nlp.vocab[line].is_stop = True
 
-    textForAnalysis = urllib.request.urlopen(documentURL).read().decode("utf8")
-    textForAnalysis = textForAnalysis.replace('\n', '').replace('"', '')
-    textForAnalysis = re.sub('\d','', textForAnalysis)
-    textForAnalysis = re.sub('[^a-zA-Z0-9^\w\s]','', textForAnalysis) 
+def extractKeywords(text, number):
+    text = text.replace('\n', ' ').replace('\r', ' ').replace('"', ' ').lower()
+    text = re.sub('\d',' ', text)
+    text = re.sub('[^a-zA-Z0-9^\w\s \']',' ', text) 
 
-    textForAnalysis = textForAnalysis.split(' ')
+    text = text.split(' ')
     words = []
 
-    for word in textForAnalysis:
+    for word in text:
         if not nlp.vocab[word].is_stop and word != '':
             words.append(word)
 
     sortedWords = Counter(words)
-    print(sortedWords.most_common(10))
 
-    return sortedWords
+    keywords = ""
+    sortedWords = sortedWords.most_common(number)
+    for word in sortedWords:
+        keywords += ", " + word[0]
+    
+    return keywords[1:]
+
+def updateJSON(jsonURL):
+    text = getTextFromURL(jsonURL)
+    qnaCollection = json.loads(text)
+
+    allwords = ""
+
+    for question in qnaCollection['questions']:
+        print(extractKeywords(question['answer'], 5)) # Answer keywords
+        print(extractKeywords(question['question'], 1)) # Question subject
+
+        allwords += question['answer']
+        allwords += question['question']
+    
+    print(extractKeywords(allwords, 10)) # Overall document keywords
+    #print (qnaCollection)
+
+def getTextFromURL(url):
+    return urllib.request.urlopen(url).read().decode("utf8")
