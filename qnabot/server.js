17d0281d62614784d8314ee5889516c02d1cdbda
Anita Ramanan <anraman@microsoft.com>
2017-09-04 17:07:30 +0100

    Added spellcheck code, commented out currently

--- qnabot/server.js
+++ qnabot/server.js
@@ -85,14 +85,74 @@ function buildResponseMessage(session, response) {
     return msg;
 }
 
+function spellcheckMessage(session, next) {
+    const request = require('request');
+    return new Promise((resolve, reject) => {
+        var query = '?text=' + session.message.text + '&mode=' + config.spellcheckMode + '&mkt=' + config.spellcheckMkt;
+        request({
+            url: config.spellcheckEndpoint + query,
+            method: 'GET',
+            headers: {
+                'Ocp-Apim-Subscription-Key': config.spellcheckKey
+            }
+        }, (error, response, body) => {
+            var result;
+            let isSuccessful = !error && response.statusCode === 200;
+
+            let spellcheckPair = {
+                'original': session.message.text,
+                'corrected': session.message.text
+            };
+
+            try {
+                if (isSuccessful) {
+                    result = JSON.parse(body);
+
+                    if (result.flaggedTokens !== null && result.flaggedTokens.length > 0) {
+                        for (var i = 0; i < result.flaggedTokens.length; i++) {
+                            spellcheckPair.corrected = spellcheckPair.corrected.replace(result.flaggedTokens[i].token, result.flaggedTokens[i].suggestions[0].suggestion);
+                        }
+                        //console.log(spellcheckPair.corrected);
+                    }
+                }
+            }
+            catch (e) {
+                error = e;
+                reject(e);
+            }
+            try {
+                if (isSuccessful) {
+                    resolve(spellcheckPair);
+                }
+                else {
+                    reject(JSON.parse(body));
+                }
+            }
+            catch (e) {
+                reject(e);
+            }
+        });
+    });
+}
+
 function setupServer() {
     server.post('/api/messages', chatConnector.listen());
     var bot = new builder.UniversalBot(chatConnector);
-
+    // Set up interceptor on all incoming messages (user -> Bot) for spellcheck
+    /*bot.use({
+        botbuilder: function (session, next) {
+             spellcheckMessage(session, next).then(
+                res => {
+                    let result = res.corrected;
+                    console.log(result);
+                    next();
+                });
+        }
+    });*/
     bot.dialog('/',
         [
             (session, args) => {
-                builder.Prompts.text(session, 'Welcome to QnA bot, you can ask questions and I\'ll lookup relivant information for you.');
+                builder.Prompts.text(session, 'Welcome to QnA bot, you can ask questions and I\'ll look up relevant information for you.');
             },
             (session, results, args) => {
                 session.replaceDialog('TopLevelQuestion', results.response);
